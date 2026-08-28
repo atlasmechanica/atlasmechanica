c95f99c7420f8793d7b01bcb33b8cb5e53987ef2
Harrison Powers <harrisonpowers@gmail.com>
2026-08-28 16:04:47 -0400

    kinematics: export four-bar foundation slice

--- packages/kinematics/src/index.ts
+++ packages/kinematics/src/index.ts
@@ -1,2 +1,6 @@
 export * from './analyticBeltAdapter.js';
+export * from './analyticFourBarAdapter.js';
 export * from './fixtures/beltDrive.js';
+export * from './fixtures/fourBar.js';
+export * from './fixtures/fourBarOracle.js';
+export * from './planarRigidBody.js';
