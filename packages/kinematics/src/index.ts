8a2d771a3d93dd0f8cb36179bf698b137f467f52
Harrison Powers <harrisonpowers@gmail.com>
2026-08-28 16:12:18 -0400

    kinematics: export reusable four-bar topology compiler

--- packages/kinematics/src/index.ts
+++ packages/kinematics/src/index.ts
@@ -3,4 +3,5 @@ export * from './analyticFourBarAdapter.js';
 export * from './fixtures/beltDrive.js';
 export * from './fixtures/fourBar.js';
 export * from './fixtures/fourBarOracle.js';
+export * from './fourBarTopology.js';
 export * from './planarRigidBody.js';
