47c01136dee1d830b88e381958ed010e75137f57
Harrison Powers <harrisonpowers@gmail.com>
2026-09-01 11:20:58 -0400

    feat(lab): add lazy four-bar runtime provider

---
+++ packages/lab/src/providers/fourBar.ts
@@ -0,0 +1,14 @@
+import {
+  analyticFourBarAdapter,
+  canonicalFourBarModel,
+} from '@atlasmechanica/kinematics';
+import { fourBarSceneCompiler } from '@atlasmechanica/scene/compilers';
+import { canonicalFourBarLab } from '../definitions.js';
+import type { MechanismLabRuntimeProvider } from '../provider.js';
+
+export const fourBarLabProvider: MechanismLabRuntimeProvider = Object.freeze({
+  definitions: Object.freeze([canonicalFourBarLab]),
+  models: Object.freeze([canonicalFourBarModel]),
+  adapters: Object.freeze([analyticFourBarAdapter]),
+  sceneCompilers: Object.freeze([fourBarSceneCompiler]),
+});
