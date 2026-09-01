ed8a441d048c5b90709aeb67d064e7b958778e43
Harrison Powers <harrisonpowers@gmail.com>
2026-09-01 15:35:04 -0400

    feat(catalog): register Brown 003

--- packages/catalog/src/catalog.ts
+++ packages/catalog/src/catalog.ts
@@ -2,11 +2,17 @@ import { createCatalog } from './schema.js';
 import { brown507Collection } from './manifests/collections/brown507.js';
 import { brown001 } from './manifests/occurrences/brown/001.js';
 import { brown002 } from './manifests/occurrences/brown/002.js';
+import { brown003 } from './manifests/occurrences/brown/003.js';
 import { crossedBeltDrive } from './manifests/subjects/crossedBeltDrive.js';
 import { openBeltDrive } from './manifests/subjects/openBeltDrive.js';
+import { quarterTurnBeltDrive } from './manifests/subjects/quarterTurnBeltDrive.js';
 
 export const collections = Object.freeze([brown507Collection] as const);
-export const subjects = Object.freeze([openBeltDrive, crossedBeltDrive] as const);
-export const occurrences = Object.freeze([brown001, brown002] as const);
+export const subjects = Object.freeze([
+  openBeltDrive,
+  crossedBeltDrive,
+  quarterTurnBeltDrive,
+] as const);
+export const occurrences = Object.freeze([brown001, brown002, brown003] as const);
 
 export const catalog = createCatalog({ collections, subjects, occurrences });
