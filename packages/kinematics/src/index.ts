4a2b032bd30be36548636d02b0a619602bdcbe4e
Harrison Powers <harrisonpowers@gmail.com>
2026-09-01 20:35:47 -0400

    refactor(kinematics): export belt continuity oracle

--- packages/kinematics/src/index.ts
+++ packages/kinematics/src/index.ts
@@ -1,6 +1,6 @@
 export * from './analyticBeltAdapter.js';
 export * from './analyticFourBarAdapter.js';
-export * from './spatialBeltAdapter.js';
+export * from './fixedAxisBeltContinuity.js';
 export * from './fixtures/beltDrive.js';
 export * from './fixtures/fourBar.js';
 export * from './fixtures/fourBarOracle.js';
