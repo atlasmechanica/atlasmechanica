0e966e67c5cacd4dbc23a32cbfe22e8294a4a009
Harrison Powers <harrisonpowers@gmail.com>
2026-09-02 23:24:57 -0400

    feat(scene): register Brown 003 projection compiler

--- packages/scene/src/compilers.ts
+++ packages/scene/src/compilers.ts
@@ -1,4 +1,5 @@
 import type { SimulationModel } from '@atlasmechanica/model';
+import { buildBrown003ProjectionScene } from './brown003Projection.js';
 import {
   buildMechanismScene as buildSchematicMechanismScene,
   type SceneBuildOptions,
@@ -26,6 +27,19 @@ export const brownBeltSceneCompiler: MechanismSceneCompiler = {
   build(options): MechanismScene {
     assertSubject(options.model, 'belt-drive', this.id);
     return buildProductionMechanismScene(options);
+  },
+};
+
+export const brown003SpatialSceneCompiler: MechanismSceneCompiler = {
+  id: 'atlas.scene.brown-003-spatial.v0',
+  supports(model): boolean {
+    return model.id === 'foundation:belt-drive:quarter-turn-guided';
+  },
+  build(options): MechanismScene {
+    if (!this.supports(options.model)) {
+      throw new TypeError(`${this.id} does not support model ${options.model.id}`);
+    }
+    return buildBrown003ProjectionScene(options);
   },
 };
 
